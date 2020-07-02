2d0522ae4f046e983e4467b05eef6fae1d7db609
chuiizeet <chuy.guerra19@outlook.es>
2020-07-01 22:23:31 -0700

    Add searchMenu

--- app/src/main/java/com/elchuy/podplay/ui/PodcastActivity.kt
+++ app/src/main/java/com/elchuy/podplay/ui/PodcastActivity.kt
@@ -1,23 +1,65 @@
 package com.elchuy.podplay.ui
 
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
 import com.elchuy.podplay.R
 import com.elchuy.podplay.repository.ItunesRepo
 import com.elchuy.podplay.service.ItunesService
 
 class PodcastActivity : AppCompatActivity() {
+
+    val TAG = javaClass.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val TAG = javaClass.simpleName
+//        val itunesService = ItunesService.instance
+//        val itunesRepo = ItunesRepo(itunesService)
+//
+//        itunesRepo.searchByTerm("Android Developer") {
+//            Log.i(TAG, "Results = $it")
+//        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_search, menu)
+        val searchMenu = menu?.findItem(R.id.search_item)
+        val searchView = searchMenu?.actionView as SearchView
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+
+        searchView.setSearchableInfo(
+            searchManager.getSearchableInfo(componentName)
+        )
+        return  true
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        handleIntent(intent!!)
+    }
+
+    private fun performSearch(term: String) {
         val itunesService = ItunesService.instance
         val itunesRepo = ItunesRepo(itunesService)
 
-        itunesRepo.searchByTerm("Android Developer") {
-            Log.i(TAG, "Results = $it")
+        itunesRepo.searchByTerm(term) {
+            Log.i(TAG, "Results: $it")
+        }
+    }
+
+    private fun handleIntent(intent: Intent) {
+        if (Intent.ACTION_SEARCH == intent.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY)
+            performSearch(query)
         }
     }
 }
